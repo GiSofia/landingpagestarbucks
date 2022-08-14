1edfa3fd4c52506b83399a087b3e3aba75e2ede9
gisofia <giulianasaborio@gmail.com>
2022-08-13 23:39:48 -0600

    Se modifica html, css y js ya que se agrega animacion con gsap

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,15 @@ toggleMenu.addEventListener('click', ()=>{
 closeMenu.addEventListener('click', ()=>{
     navMenu.classList.remove('show')
 })
+
+/* ===== GSAP ANIMATION =====*/
+//NAV
+gsap.from('.nav-logo, .nav-toggle', {opacity: 0, duration: 1, delay: 2, y:10})
+gsap.from('.nav-item, .shop-items', {opacity: 0, duration: 1, delay: 2.1, y:30, stragger: 0.2})
+
+//HOME
+gsap.from('.name, .menu-social-media', {opacity: 0, duration: 1, delay: 1.5, y:30})
+gsap.from('.menu-title', {opacity: 0, duration: 1, delay: 1.6, y:30})
+gsap.from('.menu-description', {opacity: 0, duration: 1, delay: 1.8, y:30})
+gsap.from('.menu-button', {opacity: 0, duration: 1, delay: 2.1, y:30})
+gsap.from('.menu-img', {opacity: 0, duration: 1, delay: 1.3, y:30})
